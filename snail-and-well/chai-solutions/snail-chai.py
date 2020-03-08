4007a17f7a728c4312917f5e2ef500e8a489d33e
Chai <dan.ig.rod@gmail.com>
2020-03-08 14:03:15 +0100

    Solución al ejercicio 1 + .gitignore

---
+++ snail-and-well/chai-solutions/snail-chai.py
@@ -0,0 +1,45 @@
+import operator
+
+print("Programa super awesome del caracol y el pozo")
+print("********************************************")
+
+well_height    = 125
+snail_rise     = 30
+snail_slide    = 20
+
+snail_location = 0
+total_days     = 0
+is_daytime     = False
+
+while (snail_location <= well_height):
+    is_daytime = operator.not_(is_daytime)
+    if (is_daytime):
+        snail_location += snail_rise
+        total_days += 1
+    else:
+        snail_location -= snail_slide
+    #print("Día:" if is_daytime else "Noche:" , total_days, ". El caracol está a ", snail_location)
+
+print("El caracol ha salido el día: ", total_days)
+
+
+
+
+
+
+"""
+
+         __,._
+        /  _  \                                    
+       |  6 \  \  oo                            
+        \___/ .|__||                             
+ __,..="^  . , "  , \   ---> Soy el caracol del programa. Me llamo Pepe                           
+<.__________________/
+
+
+"""
+
+
+    
+
+
