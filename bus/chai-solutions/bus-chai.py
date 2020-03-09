33f13e543042c1171545f809414362599378f5f5
Chai <dan.ig.rod@gmail.com>
2020-03-09 21:45:39 +0100

    Falta un poquito

---
+++ bus/chai-solutions/bus-chai.py
@@ -0,0 +1,44 @@
+stops = [(10, 0), (4, 1), (3, 5), (3, 4), (5, 1), (1, 5), (5, 8), (4, 6), (2, 3)]
+
+print("Stops: ", len(stops))
+passageIn,passageOut = zip(*stops)
+
+print (*passageIn)
+
+maximumOcuppation = 0
+
+total = 0
+
+for passageIn,passageOut in stops:    
+    total += passageIn - passageOut
+
+    if (total > maximumOcuppation):
+        maximumOcuppation = total
+
+print ("Max is: ", maximumOcuppation)
+print ("Avg is: ", total / len(stops), total)
+# ^^^ 
+# esto falta por afinarlo
+
+
+
+
+
+""" 
+Más maneras de pintar una array
+    
+    a = [1, 2, 3, 4, 5] 
+
+    //en bucle
+    for x in range(len(a)): 
+    print a[x], 
+    
+    //haciendo join a la string
+    print(' '.join(map(str, a)))  
+  
+    //Asterisco sirve para pintar todos los elementos de la array
+    //El separador es opcional, para separar por comas o lo que se quiera
+    print(*a, sep = ", ")  
+
+
+"""
