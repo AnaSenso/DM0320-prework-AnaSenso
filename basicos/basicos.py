23bc1583b2c8329ec13c071e301b63a799e3ea56
Chai <dan.ig.rod@gmail.com>
2020-03-10 20:32:36 +0100

    Metemos algunos comandos básicos

---
+++ basicos/basicos.py
@@ -0,0 +1,45 @@
+variable = [5,6,8]
+{
+    "caca": 2,
+    "pepe": 5,
+    "pedo": "mimimimi"
+}
+
+
+
+#BUCLES
+# indice = 1
+# while(indice < 5):
+#     indice += 1
+# print ("Hola ", indice)
+
+
+# for cacas in range(0,5):
+#     print (cacas)
+
+
+#ARRAY, LISTAS
+
+numeros    = print(len(range(0,5)))
+#print(*numeros)
+
+word = "automobile"
+numerosArray =  [2,word,5,6,7,8,98,3]
+
+
+# for elemento in  numerosArray:
+#     print(elemento)
+
+#vamos a tener que repetir hasta len(numerosArray)
+indice = len(numerosArray)
+while(indice > len(numerosArray)):     
+    print(numerosArray[indice])    
+    indice += 1
+
+
+print("El penúltimo", numerosArray[len(numerosArray) - 2])
+
+
+
+
+#print(numerosArray[1])
