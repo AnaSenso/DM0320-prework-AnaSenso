53e211179321dc702934bb693896930e8939ab81
Chai <dan.ig.rod@gmail.com>
2020-03-10 21:59:40 +0100

    resulñtado de Ana

---
+++ duel/ann-solutions/duel-ann.py
@@ -0,0 +1,31 @@
+gandalf = [10, 11, 13, 30, 22, 11, 10, 33, 22, 22]
+saruman = [23, 66, 12, 43, 12, 10, 44, 23, 12, 17]
+gandalf_win = 0
+saruman_win = 0
+
+# para cada ataque en la lista de ataques 
+    # mira el valor de la posicion (también llamada ataque) de gandalf y saruman y dime cual es mayor
+        # incrementa en 1 el xxx_win del ganador
+
+
+# si gandalf_win > saruman_win
+    #imprime Gandalf gana
+# si no:
+    # imprime Saruman gana
+
+ataqueInicial = 0
+for ataque in gandalf:
+    
+    if (gandalf[ataqueInicial] > saruman[ataqueInicial]):
+        gandalf_win += 1
+    else:
+        saruman_win += 1
+    ataqueInicial += 1
+
+if (gandalf_win > saruman_win):
+    print("Gandalf es el mejor")
+else:
+    print("Saruman es un tramposo")
+
+
+    
